efdfcc79d6cf32ee6868ef328deac0480536a418
Chris <ctjon@redhat.com>
2020-07-24 11:31:22 -0700

    ctjon - Latest Commits

--- mbp/syncScripts/sync-archive2mbp.sh
+++ mbp/syncScripts/sync-archive2mbp.sh
@@ -4,6 +4,6 @@ rsync -e ssh -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
 chris@server0:/export/archive/ \
 --exclude=.* \
 --delete-excluded \
-/Volumes/Archive
+/Volumes/Data/Archive
 
 
