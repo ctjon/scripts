c37486a9b14d7af714c5ae396d195b8c07b56a66
Chris Tjon <ctjon@redhat.com>
2021-02-04 20:30:16 -0700

    Latest Commits

--- x1carbon/backup-archive.sh
+++ x1carbon/backup-archive.sh
@@ -2,4 +2,5 @@
 rsync -e ssh -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
 --exclude=".*" \
 --delete-excluded \
-/run/media/chris/Archive/ /run/media/chris/Backup/archive
+chris@server0:/export/archive/ /opt/local/archive
+#/run/media/chris/Archive/ /run/media/chris/Backup/archive
--- x1carbon/sync-archive-from-server0.sh
+++ x1carbon/sync-archive-from-server0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRCDIR=chris@server0.internal.tjon.net:/export/archive
+SRCDIR=chris@server0.internal.tjon.net:/export/archive/
 NETDIR=/opt/local/archive
 
 rsync -e ssh -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
