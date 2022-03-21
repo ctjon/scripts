cea187fcd9f1e0d6e5ebaaafaa6b211863b16d14
Chris Tjon <ctjon@redhat.com>
2022-03-21 16:38:20 -0700

    Latest Commits

--- p51s/import-win10-p51s.sh
+++ p51s/import-win10-p51s.sh
@@ -2,7 +2,7 @@ export LIBVIRT_DEFAULT_URI=qemu:///system
 sudo virt-install --name win10kvm \
 --description 'Windows 10 KVM' \
 --ram 4096 \
---vcpus 2 \
+--vcpus 4 \
 --disk path=/home/chris/VirtualMachines/Win10x64kvm.qcow2 \
 --disk path=/usr/share/virtio-win/virtio-win.iso,device=cdrom,bus=sata \
 --os-type windows \
--- p51s/import-win11-p51s.sh
+++ p51s/import-win11-p51s.sh
@@ -2,7 +2,7 @@ export LIBVIRT_DEFAULT_URI=qemu:///system
 sudo virt-install --name Win11x64kvm \
 --description 'Windows11 x64 KVM' \
 --ram 4096 \
---vcpus 2 \
+--vcpus 4 \
 --disk path=/home/chris/VirtualMachines/Win11x64kvm.qcow2 \
 --disk path=/usr/share/virtio-win/virtio-win.iso,device=cdrom,bus=sata \
 --os-type windows \
