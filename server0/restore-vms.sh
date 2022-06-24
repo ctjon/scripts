9ca71479cc6fa24bf825198ca7de9a6861f3abf9
Chris Tjon <chris.tjon@pm.me>
2022-06-23 22:13:06 -0700

    Latest Commits

--- server0/restore-vms.sh
+++ server0/restore-vms.sh
@@ -5,7 +5,7 @@ DESTDIR=/var/lib/libvirt/images
 
 #rsync -e ssh -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
 
-rsync -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
+sudo rsync -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
 --exclude=.* \
 --delete-excluded \
 $SRCDIR/ $DESTDIR
