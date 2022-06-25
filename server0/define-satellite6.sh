504381a3830aa0592df6538e920e5a2c2ffed6ee
Chris Tjon <chris.tjon@pm.me>
2022-06-25 14:21:44 -0700

    Latest Commits

--- server0/define-satellite6.sh
+++ server0/define-satellite6.sh
@@ -1,12 +1,13 @@
 export LIBVIRT_DEFAULT_URI=qemu:///system
 sudo virt-install --name satellite6 \
 --description 'Satellite6' \
---ram 32768 \
+--ram 24576 \
 --vcpus 4 \
 --disk path=/var/lib/libvirt/images/satellite6.x86_64.qcow2,size=800 \
---os-type linux \
---os-variant rhel7.6 \
---graphics vnc \
+--disk path=/export/archive/Linux/RedHat/Satellite/satellite-6.10.0-rhel-7-x86_64-dvd.iso,device=cdrom \
+--os-variant rhel7.9 \
+--graphics spice \
 --boot hd,network,menu=on \
 --network bridge=br0,model=virtio \
 --pxe
+
