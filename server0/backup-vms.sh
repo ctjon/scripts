3e935c9e969934ddda2e58115e81148d54e7c56f
Chris Tjon <chris.tjon@pm.me>
2022-07-05 18:46:45 -0700

    Latest Commits

--- server0/backup-vms.sh
+++ server0/backup-vms.sh
@@ -10,7 +10,7 @@ sudo rsync -e ssh -aHKv --no-o --no-g --keep-dirlinks --progress --delete \
 --exclude=.* \
 --exclude=test-*.x86_64.qcow2 \
 --exclude=satclient*.x86_64.qcow2 \
---exclude=satellite6.x86_64.qcow2 \
 --delete-excluded \
 $SRCDIR/ $USBDIR
 
+#--exclude=satellite6.x86_64.qcow2 \
