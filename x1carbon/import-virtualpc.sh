09e66cf653d172809c426e04d0c375fe24506a89
Chris Tjon <chris.tjon@pm.me>
2022-10-23 08:01:50 -0700

    Latest Commits

--- x1carbon/import-virtualpc.sh
+++ x1carbon/import-virtualpc.sh
@@ -1,17 +1,17 @@
 export LIBVIRT_DEFAULT_URI=qemu:///system
-sudo virt-install --name VirtualPC \
---description 'Windows 11 VirtualPC' \
+sudo virt-install --name VirtualPC-x1carbon \
+--description 'Windows 11 VirtualPC on x1carbon' \
 --ram 4096 \
 --vcpus 4 \
---disk path=/var/lib/libvirt/images/VirtualPC.qcow2 \
+--disk path=/var/lib/libvirt/images/VirtualPC-x1carbon.qcow2 \
 --disk path=/usr/share/virtio-win/virtio-win.iso,device=cdrom,bus=sata \
 --os-type windows \
 --os-variant win10 \
 --graphics spice \
 --boot uefi \
---network type=direct,source=eth0,source_mode=bridge,model=virtio \
 --network bridge=virbr0,model=virtio \
 --sound ich9 \
 --import --noautoconsole
 
+#--network type=direct,source=eth0,source_mode=bridge,model=virtio \
 #--network bridge=br0,model=virtio \
