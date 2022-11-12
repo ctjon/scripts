e109a1cc2c476b389e49f69d1b0767257cc0d966
Chris Tjon <ctjon@redhat.com>
2022-11-12 09:28:41 -0700

    Latest Commits

--- macpro/backup-MyBackup-pCloud.sh
+++ macpro/backup-MyBackup-pCloud.sh
@@ -5,17 +5,17 @@ echo "Backup Up to $BACKUP_TARGET"
 
 touch "$BACKUP_TARGET/timestamp"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
 ~/MyDocuments/ "$BACKUP_TARGET/Crypto Folder/MyDocuments.backup"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
 ~/playbooks/ "$BACKUP_TARGET/playbooks"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
 ~/scripts/ "$BACKUP_TARGET/scripts"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
 ~/scripts.old/ "$BACKUP_TARGET/scripts.old"
 
-#rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+#rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
 #~/Pictures/ "$BACKUP_TARGET/Pictures"
