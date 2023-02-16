0faa9d20f57e498704d9f2ca87696b859628a3c3
Chris Tjon <ctjon@redhat.com>
2023-02-16 13:31:44 -0700

    Latest Commits

--- other/mkluks.sh
+++ other/mkluks.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
+ #
 echo "DONT RUN ME WITHOUT READING/MODIFYING"
 exit 0
 
-sudo fdisk /dev/sdX
-sudo parted /dev/sdX
-sudo partprobe
-cat /proc/partitions
-sudo cryptsetup luksFormat /dev/sdX1
-sudo cryptsetup luksOpen /dev/sdX1 MyEncryptedDisk
-ls -l /dev/mapper
-sudo mkfs.ext4 /dev/mapper/MyEncryptedDisk 
-sudo e2label /dev/dm-9 MyEncryptedDisk
-sudo cryptsetup luksClose /dev/mapper/MyEncryptedDisk
+lsblk
+sudo parted /dev/sdd
+sudo cryptsetup -h sha256 -c aes-xts-plain -s 256 luksFormat /dev/sdd1
+sudo cryptsetup luksOpen /dev/sdd1 MyBackupUSB
+sudo mkfs.xfs /dev/mapper/MyBackupUSB 
+sudo xfs_admin -L MyBackupUSB /dev/mapper/MyBackupUSB 
+sudo cryptsetup luksClose /dev/mapper/MyBackupUSB
