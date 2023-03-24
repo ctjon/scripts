667a4cb80f5c45dec5f1714fa884e5f264880238
Chris Tjon <ctjon@redhat.com>
2023-03-24 14:28:28 -0700

    Latest Commits

--- macpro/backup-media.sh
+++ macpro/backup-media.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rsync -aHKv --keep-dirlinks --progress --delete --delete-excluded  \
+rsync -aHKv -e ssh --keep-dirlinks --progress --delete --delete-excluded  \
 --exclude=".*" \
 --exclude="iOS-Backups" \
 --exclude="VirtualMachines" \
-/Volumes/Media/ /Volumes/Media-Backup
+/Volumes/Media/ chris@server0:/export/backup/chris-mbp/media
 
