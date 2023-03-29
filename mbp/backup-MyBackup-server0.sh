37668bd3c5cca2645dcf4f9f5255e6447030a1a3
Chris Tjon <ctjon@redhat.com>
2023-03-29 15:11:07 -0700

    Latest Commits

--- mbp/backup-MyBackup-server0.sh
+++ mbp/backup-MyBackup-server0.sh
@@ -4,25 +4,25 @@ export TGTPATH=chris@server0:/home/chris/chris-mbp
 echo "syncing Home Directory to Server0"
 ssh chris@server0 "mkdir $TGTPATH"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 ~chris/MyDocuments/ $TGTPATH/MyDocuments
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 ~chris/playbooks/ $TGTPATH/playbooks
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 ~chris/scripts/ $TGTPATH/scripts
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 ~chris/Books/ $TGTPATH/Books
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 ~chris/Pictures/ $TGTPATH/Pictures
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 /Volumes/Media/Music/ $TGTPATH/Music
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
 /Volumes/Media/HomeVideos/ $TGTPATH/HomeVideos
 
 touch $TGTPATH/timestamp
