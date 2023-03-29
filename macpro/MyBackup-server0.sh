163be392f8090904f2935b49ff79137a275286b5
Chris Tjon <ctjon@redhat.com>
2023-03-29 15:05:27 -0700

    Latest Commits

---
+++ macpro/MyBackup-server0.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "********************************************************************************"
+export TGTPATH=chris@server0:~/MyBackup-mbp
+echo "syncing Home Directory to USB"
+mkdir  $TGTPATH/homedir
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+~chris/MyDocuments/ $TGTPATH/homedir/MyDocuments
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+~chris/playbooks/ $TGTPATH/homedir/playbooks
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+~chris/scripts/ $TGTPATH/homedir/scripts
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+~chris/Books/ $TGTPATH/homedir/Books
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+~chris/Pictures/ $TGTPATH/homedir/Pictures
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+/Volumes/Media/Music/ $TGTPATH/Music
+
+rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+/Volumes/Media/HomeVideos/ $TGTPATH/HomeVideos
+
+touch $TGTPATH/homedir/timestamp
