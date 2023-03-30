10cb2589fc8b505144478374ebc63cffcd3f0891
Chris Tjon <ctjon@redhat.com>
2023-03-29 18:04:32 -0700

    Latest Commits

--- mbp/backup-MyBackup-server0.sh
+++ mbp/backup-MyBackup-server0.sh
@@ -7,24 +7,24 @@ echo "syncing Home Directory to Server0"
 ssh $SSH_USER mkdir $TGTPATH
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/MyDocuments/ $SSH_USER:$TGTPATH/MyDocuments
+~/MyDocuments/ $SSH_USER:$TGTPATH/MyDocuments
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/playbooks/ $SSH_USER:$TGTPATH/playbooks
+~/playbooks/ $SSH_USER:$TGTPATH/playbooks
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/scripts/ $SSH_USER:$TGTPATH/scripts
+~/scripts/ $SSH_USER:$TGTPATH/scripts
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/Books/ $SSH_USER:$TGTPATH/Books
+~/Books/ $SSH_USER:$TGTPATH/Books
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/Pictures/ $SSH_USER:$TGTPATH/Pictures
+~/Pictures/ $SSH_USER:$TGTPATH/Pictures
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-/Volumes/Media/Music/ $SSH_USER:$TGTPATH/Music
+~/Music/ $SSH_USER:$TGTPATH/Music
 
 rsync -e ssh -aHKvz --delete --delete-excluded --delete-during --progress \
-/Volumes/Media/HomeVideos/ $SSH_USER:$TGTPATH/HomeVideos
+~/Movies/TV/Home\ Videos/ $SSH_USER:$TGTPATH/HomeVideos
 
 ssh $SSH_USER touch $TGTPATH/timestamp
--- mbp/backup-MyBackupUSB.sh
+++ mbp/backup-MyBackupUSB.sh
@@ -5,24 +5,24 @@ echo "syncing Home Directory to USB"
 mkdir  $USBPATH/homedir
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/MyDocuments/ $USBPATH/homedir/MyDocuments
+~/MyDocuments/ $USBPATH/homedir/MyDocuments
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/playbooks/ $USBPATH/homedir/playbooks
+~/playbooks/ $USBPATH/homedir/playbooks
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/scripts/ $USBPATH/homedir/scripts
+~/scripts/ $USBPATH/homedir/scripts
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/Books/ $USBPATH/homedir/Books
+~/Books/ $USBPATH/homedir/Books
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~chris/Pictures/ $USBPATH/homedir/Pictures
+~/Pictures/ $USBPATH/homedir/Pictures
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-/Volumes/Media/Music/ $USBPATH/Music
+~/Music/ $USBPATH/Music
 
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-/Volumes/Media/HomeVideos/ $USBPATH/HomeVideos
+~/Movies/TV/Home\ Videos/ $USBPATH/HomeVideos
 
 touch $USBPATH/homedir/timestamp
