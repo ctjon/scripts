e42eeae3a3f5274dd28c0ff1adb7956fa418e648
Chris Tjon <chris@server0.internal.tjon.net>
2023-10-25 15:38:12 -0700

    Latest Commits

--- kvm/define-satellite6-rhel8.sh
+++ kvm/define-satellite6-rhel8.sh
@@ -1,7 +1,7 @@
 export LIBVIRT_DEFAULT_URI=qemu:///system
 sudo virt-install --name satellite6 \
 --description 'Satellite6-RHEL8' \
---ram 24576 \
+--ram 32768 \
 --vcpus 4 \
 --disk path=/var/lib/libvirt/images/satellite6.qcow2,size=512 \
 --disk path=/export/archive/Linux/RedHat/Satellite/Satellite-6.13.4-rhel-8-x86_64.dvd.iso,device=cdrom \
