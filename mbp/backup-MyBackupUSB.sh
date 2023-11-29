282d746798dc83a5fea93378ee6a4261f74544ed
Chris Tjon <ctjon@redhat.com>
2023-11-29 15:54:20 -0700

    Latest Commits

--- mbp/backup-MyBackupUSB.sh
+++ mbp/backup-MyBackupUSB.sh
@@ -4,31 +4,38 @@ export USBPATH=/Volumes/MyBackupUSB
 echo "syncing Home Directory to USB"
 mkdir  $USBPATH/homedir
 
+echo "Backing up MyDocuments"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/MyDocuments/ $USBPATH/homedir/MyDocuments
 
+echo "Backing up playbooks"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/playbooks/ $USBPATH/homedir/playbooks
 
+echo "Backing up scripts"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/scripts/ $USBPATH/homedir/scripts
 
+echo "Backing up Music"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/Music/ $USBPATH/homedir/Music
 
+echo "Backing up Books"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
-~/pCloud\ Drive/Books/ $USBPATH/homedir/Books
+~/Books/ $USBPATH/homedir/Books
 #~/Library/Mobile\ Documents/com~apple~CloudDocs/Books/ $USBPATH/homedir/Books
 
+echo "Backing up Pictures"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/Pictures/ $USBPATH/homedir/Pictures
 
+echo "Backing up HomeVideos"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 --exclude=.DS_Store \
 ~/Movies/TV/Media.localized/Home\ Videos/ $USBPATH/HomeVideos
