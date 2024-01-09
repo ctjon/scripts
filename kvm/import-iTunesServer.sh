6b8ab82bf6b82a5c565718a2d3ae4280bf11067d
Chris Tjon <chris@server0.internal.tjon.net>
2024-01-09 16:36:00 -0700

    Latest Commits

--- kvm/import-iTunesServer.sh
+++ kvm/import-iTunesServer.sh
@@ -4,12 +4,12 @@ sudo virt-install --name iTunesServer \
 --ram 4096 \
 --vcpus 4 \
 --disk path=/var/lib/libvirt/images/iTunesServer.qcow2 \
---disk path=/dev/ssdvg/ituneslv \
+--disk path=/dev/mediavg/ituneslv \
 --os-variant win10 \
---graphics spice \
+--graphics vnc \
 --sound ich9 \
 --boot uefi \
---network bridge=br0,model=virtio \
+--network network=macvtap-bridge,model=e1000 \
 --import --noautoconsole
 
 #--boot hd,network,menu=on \
