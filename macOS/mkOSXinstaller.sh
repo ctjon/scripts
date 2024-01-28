f04a9fb315f8055f18e9dd66ad0297cdfb5d541f
Chris Tjon <ctjon@redhat.com>
2024-01-28 07:16:26 -0700

    Latest Commits

--- macOS/mkOSXinstaller.sh
+++ macOS/mkOSXinstaller.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#SRCDIR=/Volumes/Archive/MacOSX/Apple/13.2.1-macOS-Ventura
-#SRCDIR=/Applications
-SRCDIR=/Users/chris/Downloads
+SRCDIR=/Applications
+#SRCDIR=/Users/chris/Downloads
 DEST=/Volumes/Untitled
 
 sudo $SRCDIR/Install\ macOS\ Sonoma.app/Contents/Resources/createinstallmedia --volume $DEST --nointeraction
