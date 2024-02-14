0eee28c4974b54984af00a8bfc4ce8e8a5052dea
Chris Tjon <ctjon@redhat.com>
2024-02-14 09:20:32 -0700

    Latest Commits

--- mbp/backup-MyDocs2pCloud.sh
+++ mbp/backup-MyDocs2pCloud.sh
@@ -5,6 +5,7 @@ echo "Backup Up to $BACKUP_TARGET"
 
 touch "$BACKUP_TARGET/timestamp"
 
-rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-times \
+#rsync -aHKvz --delete --delete-excluded --delete-during --progress --no-p --no-o --no-g --no-times \
+rsync -rltvzx  --delete --delete-excluded --delete-during --progress --no-p --no-o --no-g --no-times \
 --exclude=.DS_Store \
-~/MyDocuments/ "$BACKUP_TARGET/Crypto Folder/MyDocuments.backup"
+~/MyDocuments/ "$BACKUP_TARGET/Crypto Folder/MyDocuments"
