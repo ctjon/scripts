5062fbfc371f5d1c3b1c7daa578a08ba15949043
Chris Tjon <ctjon@redhat.com>
2024-03-23 07:55:47 -0700

    Latest Commits

--- mbp/backup-MyBackupUSB.sh
+++ mbp/backup-MyBackupUSB.sh
@@ -19,7 +19,7 @@ rsync -aHKvz --delete --delete-excluded --delete-during --progress \
 
 echo "Backing up ThinkOrSwim"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
-~/.[^.]*  $USBPATH/homedir/thinkorswim
+~/thinkorswim  $USBPATH/homedir/thinkorswim
 
 echo "Backing up playbooks"
 rsync -aHKvz --delete --delete-excluded --delete-during --progress \
