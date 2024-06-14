f8c404b451ab73364725701864a27657ac84bfc4
Chris Tjon <ctjon@redhat.com>
2024-06-13 20:26:53 -0700

    Latest Commits

--- mba/backup-vms.sh
+++ mba/backup-vms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -aHKv --keep-dirlinks --progress --delete --exclude=".*" --delete-excluded /Volumes/mbp-data/VirtualMachines/ /Volumes/Media/VirtualMachines
+rsync -aHKv --keep-dirlinks --progress --delete --exclude=".*" --delete-excluded ~/Parallels/ /Volumes/VM-Backups/mba
