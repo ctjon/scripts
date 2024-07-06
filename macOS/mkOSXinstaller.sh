562244fc94efbb442807c76bddfcba4f8b678bf3
Chris Tjon <ctjon@redhat.com>
2024-07-06 12:32:55 -0700

    Latest Commits

--- macOS/mkOSXinstaller.sh
+++ macOS/mkOSXinstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-SRCDIR=/Applications
-#SRCDIR=/Users/chris/Downloads
+#SRCDIR=/Applications
+SRCDIR=/Users/chris/Downloads
 DEST=/Volumes/Untitled
 
 sudo $SRCDIR/Install\ macOS\ Sonoma.app/Contents/Resources/createinstallmedia --volume $DEST --nointeraction
