6e091e83746d36f2e7b4e63d2c895523c1db1508
Chris Tjon <ctjon@redhat.com>
2024-07-06 11:59:28 -0700

    Latest Commits

--- macOS/mk-monterey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SRCDIR=/Volumes/4TB-NVMe/Archive/MacOSX/Apple/12.6.2-macOS-Monterey
-#SRCDIR=/Applications
-#SRCDIR=/Users/chris/Downloads
-DEST=/Volumes/Untitled
-
-sudo $SRCDIR/Install\ macOS\ Monterey.app/Contents/Resources/createinstallmedia --volume $DEST --nointeraction
