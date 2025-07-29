5ec09fa75914cc98a70acc066ac19512a8bc7d86
Chris Tjon <ctjon@redhat.com>
2025-07-28 19:09:39 -0500

    Latest Commits

--- mba/backupMyDocuments2ProtonDrive.sh
+++ mba/backupMyDocuments2ProtonDrive.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-USB_DISK=/Volumes/MyBackupUSB
+export PROTON_DRIVE=/Users/chris/Library/CloudStorage/ProtonDrive-chris.tjon@pm.me-folder
 
 echo "********************************************************************************"
-export USB_TGT=$USB_DISK/MyDocuments.backup
-echo "Backup MyDocuments to $USB_TGT"
+echo "Backup MyDocuments to $PROTON_DRIVE/MyDocuments.backup"
 rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
 --progress --exclude=.DS_Store \
-/Users/chris/MyDocuments/ /Users/chris/Library/CloudStorage/ProtonDrive-chris.tjon@pm.me-folder/MyDocuments.backup
+/Users/chris/MyDocuments/ $PROTON_DRIVE/MyDocuments.backup
 
+echo "********************************************************************************"
+echo "Backup scripts to $PROTON_DRIVE/scripts.backup"
+rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
+--progress --exclude=.DS_Store \
+"/Users/chris/scripts/" $PROTON_DRIVE/scripts.backup
 
+echo "********************************************************************************"
+echo "Backup playbooks to $PROTON_DRIVE/playbooks.backup"
+rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
+--progress --exclude=.DS_Store \
+"/Users/chris/playbooks/" $PROTON_DRIVE/playbooks.backup
