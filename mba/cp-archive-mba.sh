417b4c9fdea6530a11ac0099fc3933d6f150bde7
Chris Tjon <ctjon@redhat.com>
2025-08-06 14:09:51 -0500

    Latest Commits

--- mba/cp-archive-mba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rsync -aHKv -e ssh --keep-dirlinks --progress --delete --delete-excluded  \
---exclude=".*" \
---exclude="\@*" \
---exclude="\#*" \
-chris@server0:/export/archive/ /Volumes/Archive
-
---
+++ mba/cp-archive2server0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rsync -aiHKvz -e ssh --progress --delete --delete-during --delete-excluded \
+  --exclude=".*" \
+  --exclude=".DS_Store" \
+  --exclude="@*" \
+  --exclude="#*" \
+  --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r \
+/Volumes/Archive/ chris@server0:/export/archive
+
