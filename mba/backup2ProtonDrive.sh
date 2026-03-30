a31eef336119cf8fe896e66546adff3e4c2883c3
Chris Tjon <ctjon@redhat.com>
2026-03-30 15:45:27 -0700

    Latest Commits

--- mba/backup2ProtonDrive.sh
+++ mba/backup2ProtonDrive.sh
@@ -24,13 +24,6 @@ rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
 ~/Library/Mobile\ Documents/com~apple~CloudDocs/HomeVideos/ $BKUP_TGT
 
 echo "********************************************************************************"
-export BKUP_TGT=$BKUP_DISK/Music.backup
-echo "Backup Music to $BKUP_TGT"
-rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
---progress --exclude=.DS_Store \
-~/Music/ $BKUP_TGT
-
-echo "********************************************************************************"
 export BKUP_TGT=$BKUP_DISK/scripts.backup
 echo "Backup scripts to $BKUP_TGT"
 rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
@@ -51,23 +44,23 @@ rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
 --progress --exclude=.DS_Store \
 ~/thinkorswim/ $BKUP_TGT
 
-echo "********************************************************************************"
-export BKUP_TGT=$BKUP_DISK/dotfiles.backup
-echo "Backup Config Files to $BKUP_TGT"
-rsync -aHKvz --delete --delete-excluded --delete-during --progress \
---exclude=.DS_Store \
---exclude=.Trash \
---exclude=.zsh_sessions \
---exclude=.thinkorswim \
-~/.??*  $BKUP_TGT
-#~/.[^.]*  $BKUP_TGT
-##--exclude=.config/containers \
-##--exclude=.local/share/containers \
+#echo "********************************************************************************"
+#export BKUP_TGT=$BKUP_DISK/dotfiles.backup
+#echo "Backup Config Files to $BKUP_TGT"
+#rsync -aHKvz --delete --delete-excluded --delete-during --progress \
+#--exclude=.DS_Store \
+#--exclude=.Trash \
+#--exclude=.zsh_sessions \
+#--exclude=.thinkorswim \
+#~/.??*  $BKUP_TGT
+##~/.[^.]*  $BKUP_TGT
+###--exclude=.config/containers \
+###--exclude=.local/share/containers \
 
-echo "********************************************************************************"
-export BKUP_TGT=$BKUP_DISK/Pictures.backup
-echo "Backup Pictures to $BKUP_TGT"
-rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
---progress --exclude=.DS_Store \
-~/Pictures/ $BKUP_TGT
+#echo "********************************************************************************"
+#export BKUP_TGT=$BKUP_DISK/Pictures.backup
+#echo "Backup Pictures to $BKUP_TGT"
+#rsync -aHKvzO --delete --delete-excluded --delete-during --no-o --no-g \
+#--progress --exclude=.DS_Store \
+#~/Pictures/ $BKUP_TGT
 
